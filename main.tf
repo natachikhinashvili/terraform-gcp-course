4b9ef863753155c49fa9c434a7b149e0728771d8
natatchikhinashvili <natanatuka33@gmail.com>
2024-03-25 17:38:44 +0400

    feat: tf files

---
+++ main.tf
@@ -0,0 +1,4 @@
+provider "google"{
+    project = "nameofproject"
+    region = "us-central1"
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "account_id"{
+    type = string
+}
+variable "projectname"{
+    type = string
+}
+variable "instancename"{
+    type = string
+}
+
+variable "account_id"{
+    type = string
+}
